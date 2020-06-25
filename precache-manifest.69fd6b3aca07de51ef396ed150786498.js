31a898a07ff4b701073ec3d7ad2519f19a9cfab1
prerna-p <prernasocialid@gmail.com>
2020-06-25 14:51:26 -0400

    Updates

---
+++ precache-manifest.69fd6b3aca07de51ef396ed150786498.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movie-webapp-client/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "e597403aef8d32f25c88",
+    "url": "/movie-webapp-client/static/css/main.82465277.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movie-webapp-client/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "73dcdf485e94575910b3",
+    "url": "/movie-webapp-client/static/js/1.73dcdf48.chunk.js"
+  },
+  {
+    "revision": "ff001de49abefdaf34da",
+    "url": "/movie-webapp-client/static/js/runtime~main.ff001de4.js"
+  },
+  {
+    "revision": "e597403aef8d32f25c88",
+    "url": "/movie-webapp-client/static/js/main.e597403a.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movie-webapp-client/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movie-webapp-client/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movie-webapp-client/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "73dcdf485e94575910b3",
+    "url": "/movie-webapp-client/static/css/1.1c325c26.chunk.css"
+  },
+  {
+    "revision": "a15493896de6101422e02ccfa2ff35d2",
+    "url": "/movie-webapp-client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-webap-client/precache-manifest.0da602fdace000f671b6f0807f08cecf.js"
+  "/movie-webapp-client/precache-manifest.69fd6b3aca07de51ef396ed150786498.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/movie-webap-client/index.html", {
+workbox.routing.registerNavigationRoute("/movie-webapp-client/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
